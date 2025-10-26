425e683bfda86330b147753d5d748dbd52f58672
edgar.balderas <ebg_automation@outlook.com>
2025-10-25 22:19:18 -0600

    Add BaseTest

---
+++ src/test/java/com/company/qa/tests/base/BaseTest.java
@@ -0,0 +1,27 @@
+package com.company.qa.tests.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
--- src/test/java/com/company/qa/tests/login/LoginTests.java
+++ src/test/java/com/company/qa/tests/login/LoginTests.java
@@ -1,33 +1,12 @@
 package com.company.qa.tests.login;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.company.qa.tests.base.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
-public class LoginTests {
-
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().clearDriverCache().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com/");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+public class LoginTests extends BaseTest {
 
     @Test
     public void testValidLogin() {
@@ -55,5 +34,4 @@ public class LoginTests {
             "Login failed or incorrect page title."
         );
     }
-
 }
